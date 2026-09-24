367107c41526af8262649a7508252e81eb7e432c
dancingmirrors <dancingmirrors@icloud.com>
2026-09-23 23:10:03 -0600

    drm/nouveau: Fix the style for the r570 fb header.

--- drivers/gpu/drm/nouveau/nvkm/subdev/gsp/rm/r570/nvrm/fb.h
+++ drivers/gpu/drm/nouveau/nvkm/subdev/gsp/rm/r570/nvrm/fb.h
@@ -9,20 +9,20 @@
 /* Excerpt of RM headers from https://github.com/NVIDIA/open-gpu-kernel-modules/tree/570.144 */
 
 typedef struct NV2080_CTRL_INTERNAL_MEMSYS_GET_STATIC_CONFIG_PARAMS {
-	NvBool bOneToOneComptagLineAllocation;
-	NvBool bUseOneToFourComptagLineAllocation;
-	NvBool bUseRawModeComptaglineAllocation;
-	NvBool bDisableCompbitBacking;
-	NvBool bDisablePostL2Compression;
-	NvBool bEnabledEccFBPA;
-	NvBool bL2PreFill;
-	NV_DECLARE_ALIGNED(NvU64 l2CacheSize, 8);
-	NvBool bFbpaPresent;
-	NvU32  comprPageSize;
-	NvU32  comprPageShift;
-	NvU32  ramType;
-	NvU32  ltcCount;
-	NvU32  ltsPerLtcCount;
+    NvBool bOneToOneComptagLineAllocation;
+    NvBool bUseOneToFourComptagLineAllocation;
+    NvBool bUseRawModeComptaglineAllocation;
+    NvBool bDisableCompbitBacking;
+    NvBool bDisablePostL2Compression;
+    NvBool bEnabledEccFBPA;
+    NvBool bL2PreFill;
+    NV_DECLARE_ALIGNED(NvU64 l2CacheSize, 8);
+    NvBool bFbpaPresent;
+    NvU32  comprPageSize;
+    NvU32  comprPageShift;
+    NvU32  ramType;
+    NvU32  ltcCount;
+    NvU32  ltsPerLtcCount;
 } NV2080_CTRL_INTERNAL_MEMSYS_GET_STATIC_CONFIG_PARAMS;
 
 #define NV2080_CTRL_CMD_INTERNAL_MEMSYS_GET_STATIC_CONFIG (0x20800a1c)
@@ -30,20 +30,20 @@ typedef struct NV2080_CTRL_INTERNAL_MEMSYS_GET_STATIC_CONFIG_PARAMS {
 #define NV0080_CTRL_CMD_FB_GET_COMPBIT_STORE_INFO (0x801306)
 
 typedef struct NV0080_CTRL_FB_GET_COMPBIT_STORE_INFO_PARAMS {
-	NV_DECLARE_ALIGNED(NvU64 Size, 8);
-	NV_DECLARE_ALIGNED(NvU64 Address, 8);
-	NvU32  AddressSpace;
-	NvU32  MaxCompbitLine;
-	NvU32  comptagsPerCacheLine;
-	NvU32  cacheLineSize;
-	NvU32  cacheLineSizePerSlice;
-	NvU32  cacheLineFetchAlignment;
-	NV_DECLARE_ALIGNED(NvU64 backingStoreBase, 8);
-	NvU32  gobsPerComptagPerSlice;
-	NvU32  backingStoreCbcBase;
-	NvU32  comptaglineAllocationPolicy;
-	NV_DECLARE_ALIGNED(NvU64 privRegionStartOffset, 8);
-	NvU32  cbcCoveragePerSlice;
+    NV_DECLARE_ALIGNED(NvU64 Size, 8);
+    NV_DECLARE_ALIGNED(NvU64 Address, 8);
+    NvU32  AddressSpace;
+    NvU32  MaxCompbitLine;
+    NvU32  comptagsPerCacheLine;
+    NvU32  cacheLineSize;
+    NvU32  cacheLineSizePerSlice;
+    NvU32  cacheLineFetchAlignment;
+    NV_DECLARE_ALIGNED(NvU64 backingStoreBase, 8);
+    NvU32  gobsPerComptagPerSlice;
+    NvU32  backingStoreCbcBase;
+    NvU32  comptaglineAllocationPolicy;
+    NV_DECLARE_ALIGNED(NvU64 privRegionStartOffset, 8);
+    NvU32  cbcCoveragePerSlice;
 } NV0080_CTRL_FB_GET_COMPBIT_STORE_INFO_PARAMS;
 
 #define NV0080_CTRL_CMD_FB_GET_COMPBIT_STORE_INFO_ADDRESS_SPACE_UNKNOWN 0
